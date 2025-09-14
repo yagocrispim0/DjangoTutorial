cde376d0d14084fbaec98a7a48ffc87fd3790bd7
yagocrispim0 <yagocrgomes@gmail.com>
2025-09-14 11:41:14 -0300

    Tutorial 5

--- ppolls/models.py
+++ ppolls/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     
     def was_published_recently(self):
         now = timezone.now()
-        return self.pub_date >= now - datetime.timedelta(days=1)
+        return now >= self.pub_date >= now - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
